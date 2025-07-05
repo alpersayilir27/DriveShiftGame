fb1ec9e215c7babf7eede4104275a76030748e1a
Alper <malper.sayilir@std.hku.edu.tr>
2025-07-05 21:49:44 +0300

    v0.4 Race Manager Added

---
+++ Assets/Scripts/RaceSystem/RaceManager.cs
@@ -0,0 +1,186 @@
+using JetBrains.Rider.Unity.Editor;
+using TMPro;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    public static RaceManager Instansce;
+    [SerializeField] private TextMeshProUGUI currentLapTimeText;
+    [SerializeField] private TextMeshProUGUI overallRaceTimeText;
+    [SerializeField] private TextMeshProUGUI bestLapTimeText;
+    [SerializeField] private TextMeshProUGUI lapText;
+    [SerializeField] private TextMeshProUGUI checkpointMissedText;
+    [SerializeField] private GameObject[] checkpoints;
+    [SerializeField] private int lastCheckpointIndex = -1;
+    [SerializeField] private bool isCircuit = false;
+    [SerializeField] private int totalLaps = 3;
+    [SerializeField] private int currentLap = 1;
+    private bool raceStarted = false;
+    private bool raceFinished = false;
+    private bool ifCheckpointMissed = false;
+
+    private float currentLapTime = 0f;
+    private float overallRaceTime = 0f;
+    private float bestLapTime = Mathf.Infinity;
+
+    private void Awake()
+    {
+        if (Instansce == null)
+        {
+            Instansce = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        if (raceStarted)
+        {
+            UpdateTimers();
+        }
+        UpdateUI();
+    }
+
+    public void CheckpointReached(int checkpointIndex)
+    {
+        if ((!raceStarted && checkpointIndex != 0) || raceFinished) return;
+
+        bool validLapFinish = isCircuit && raceStarted && lastCheckpointIndex == checkpoints.Length - 1 && checkpointIndex == 0;
+
+        if (checkpointIndex == 0 && !raceStarted)
+        {
+            StartRace();
+            lastCheckpointIndex = checkpointIndex;
+            HideCheckpointMissedText();
+            return;
+        }
+
+        if (checkpointIndex == lastCheckpointIndex + 1 || validLapFinish)
+        {
+            UpdateCheckpoint(checkpointIndex);
+            HideCheckpointMissedText();
+        }
+        else
+        {
+            ShowCheckpointMissedText();
+        }
+    }
+
+
+    private void UpdateCheckpoint(int checkpointIndex)
+    {
+        if (checkpointIndex == 0)
+        {
+            if (!raceStarted)
+            {
+                StartRace();
+            }
+            else
+            {
+                OnLapFinished();
+            }
+        }
+        else if (!isCircuit && checkpointIndex == checkpoints.Length - 1)
+        {
+            OnLapFinished();
+        }
+
+        lastCheckpointIndex = checkpointIndex;
+    }
+
+
+    private void OnLapFinished()
+    {
+        currentLap++;
+
+        if (currentLapTime < bestLapTime)
+        {
+            bestLapTime = currentLapTime;
+        }
+
+        if (currentLap > totalLaps)
+        {
+            EndRace();
+        }
+        else
+        {
+            currentLapTime = 0f;
+            lastCheckpointIndex = isCircuit ? 0 : -1;
+        }
+
+        
+    }
+
+    private void StartRace()
+    {
+        raceStarted = true;
+        raceFinished = false;
+    }
+
+    private void EndRace()
+    {
+        raceFinished = true;
+        raceStarted = false;
+    }
+
+    private void UpdateTimers()
+    {
+        currentLapTime += Time.deltaTime;
+        overallRaceTime += Time.deltaTime;
+
+    }
+
+    private void UpdateUI()
+    {
+        currentLapTimeText.text ="Current Lap Time: "+ FormatTime(currentLapTime);
+        overallRaceTimeText.text ="Overall Race Time: "+ FormatTime(overallRaceTime);
+        lapText.text = $"Lap {currentLap}/{totalLaps}";
+        bestLapTimeText.text = "Best Lap Time: " +FormatTime(bestLapTime);
+        UpdateCheckpointMissedText();
+    }
+
+    private void UpdateCheckpointMissedText()
+    {
+        if (ifCheckpointMissed)
+        {
+            float alpha = Mathf.PingPong(Time.time * 2, 1);
+            Color newColor = checkpointMissedText.color;
+            newColor.a = alpha;
+            checkpointMissedText.color = newColor;
+        }
+    }
+    
+    private void ShowCheckpointMissedText()
+    {
+        if (!ifCheckpointMissed)
+        {
+            checkpointMissedText.gameObject.SetActive(true);
+            ifCheckpointMissed = true;
+        }
+    }
+
+    private void HideCheckpointMissedText()
+    {
+        if (ifCheckpointMissed)
+        {
+            checkpointMissedText.gameObject.SetActive(false);
+            ifCheckpointMissed = false;
+        } 
+    }
+
+    private string FormatTime(float time)
+    {
+        if (float.IsInfinity(time) || time < 0) return "--:--";
+
+        int minutes = (int)time / 60;
+        float seconds = time % 60;
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+
+
+}
